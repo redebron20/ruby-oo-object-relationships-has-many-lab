70cf320f7013055b9b7d53258b3239deb335779c
redebron20 <redebron20@gmail.com>
2020-11-15 00:04:48 -0500

    commit tree b6157ef55a01d4f8c8164b4fd600812222076a05

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Artist
+
+    attr_accessor :name
+
+    @@song_count = 0
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def add_song(song)
+        @songs << song
+        song.artist = self
+    end
+
+    def add_song_by_name(name)      
+        song = Song.new(name)
+        self.songs << song
+        song.artist = self
+    end
+   
+    def self.song_count
+        Song.all.count
+    end
+
+end
